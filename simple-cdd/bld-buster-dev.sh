b365521529cee7391806d932f193bbc581d2f1ca
Andy Sizer <andy@unassigned.unassigned>
2020-05-01 12:08:18 +0700

    Just pick up packages

--- simple-cdd/bld-buster-dev.sh
+++ simple-cdd/bld-buster-dev.sh
@@ -4,4 +4,4 @@
 export DISKTYPE=DVD
 
 build-simple-cdd --dist buster -p deb-buster-dev -a deb-buster-dev --verbose --debug --logfile ./deb-buster-dev.log \
-		 --local-packages "$(ls ~/apt/extra-packages/*)"
+		 --local-packages "$(ls ~/apt/extra-packages/*\.deb)"
