15a84e7afc617546d199c864a03227999fe42025
Andy Sizer <andysizer@gmail.com>
2020-05-03 12:15:37 +0700

    set up virtualenvwrapper

--- ansible/scripts/bld-deb-buster-dev.sh
+++ ansible/scripts/bld-deb-buster-dev.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 export ANSIBLE_CONFIG=./ansible.cfg
+
 INITUSER=../playbooks/init_user.yml
 PASSWORDLESS=../playbooks/passwordless.yml
+VIRTUALENV=../playbooks/python3_virtualenv.yml
 
 ansible-playbook -K --become-method=su -e user=${USER} ${INITUSER}
 ansible-playbook -e user=${USER} ${PASSWORDLESS}
+ansible-playbook -e user=${USER} ${VIRTUALENV}
