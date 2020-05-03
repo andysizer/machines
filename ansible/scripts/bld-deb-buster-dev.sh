31444dff3c1530c5e2554b35083504af07de7f90
Andy Sizer <andysizer@gmail.com>
2020-05-03 16:30:06 +0700

    Set up manim

--- ansible/scripts/bld-deb-buster-dev.sh
+++ ansible/scripts/bld-deb-buster-dev.sh
@@ -5,7 +5,10 @@ export ANSIBLE_CONFIG=./ansible.cfg
 INITUSER=../playbooks/init_user.yml
 PASSWORDLESS=../playbooks/passwordless.yml
 VIRTUALENV=../playbooks/python3_virtualenv.yml
+MANIM=../playbooks/manim_setup.yml
 
 ansible-playbook -K --become-method=su -e user=${USER} ${INITUSER}
 ansible-playbook -e user=${USER} ${PASSWORDLESS}
 ansible-playbook -e user=${USER} ${VIRTUALENV}
+ansible-playbook -e 'user=${USER} venv=manim-priv repo=git@github.com:andysizer/manim.git' ${MANIM}
+
