22dc68b193332aaf2cd1098eab5964fb1ada2dae
Andy Sizer <andysizer@gmail.com>
2020-05-03 11:07:13 +0700

    Sort out authentication
    Assume sudoers not set up for ${USER}. The first playbook set this up
    and so uses 'su'. After that we can use 'sudo'.

--- ansible/scripts/bld-deb-buster-dev.sh
+++ ansible/scripts/bld-deb-buster-dev.sh
@@ -4,5 +4,5 @@ export ANSIBLE_CONFIG=./ansible.cfg
 INITUSER=../playbooks/init_user.yml
 PASSWORDLESS=../playbooks/passwordless.yml
 
-ansible-playbook --become-method su --ask-su-pass ${INITUSER}
-ansible-playbook ${PASSWORDLESS}
+ansible-playbook -K --become-method=su -e user=${USER} ${INITUSER}
+ansible-playbook -e user=${USER} ${PASSWORDLESS}
