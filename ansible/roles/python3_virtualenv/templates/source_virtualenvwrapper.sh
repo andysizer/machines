71fe44a3da1130430c263509c4697d6859016300
Andy Sizer <andysizer@gmail.com>
2020-05-03 16:29:13 +0700

    Add WORKON_HOME to .bashrc

--- ansible/roles/python3_virtualenv/templates/source_virtualenvwrapper.sh
+++ ansible/roles/python3_virtualenv/templates/source_virtualenvwrapper.sh
@@ -3,14 +3,16 @@
 VENVSCRIPT=/usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
 
 COMMENT="# Added by Ansible role 'python3_virtualwrapper'"
-STATEMENT="source ${VENVSCRIPT}"
+STATEMENT1="source ${VENVSCRIPT}"
+STATEMENT2="export WORKON_HOME=$HOME/.virtualenvs"
 BASHRC=~{{ user }}/.bashrc
 
 if ! grep -q "${COMMENT}" ${BASHRC} ; then
     echo "Setting up virtualenvwrapper..."
     echo "" >> ${BASHRC}
     echo ${COMMENT} >> ${BASHRC}
-    echo ${STATEMENT} >> ${BASHRC}
+    echo ${STATEMENT1} >> ${BASHRC}
+    echo ${STATEMENT2} >> ${BASHRC}
 fi
 
 
