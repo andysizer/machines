6e14064ff9087f771532c3368c807661fee1c413
Andy Sizer <andysizer@gmail.com>
2020-05-04 15:16:53 +0700

    Add pycharm_seup role

--- ansible/scripts/bld-deb-buster-dev.sh
+++ ansible/scripts/bld-deb-buster-dev.sh
@@ -6,9 +6,11 @@ INITUSER=../playbooks/init_user.yml
 PASSWORDLESS=../playbooks/passwordless.yml
 VIRTUALENV=../playbooks/python3_virtualenv.yml
 MANIM=../playbooks/manim_setup.yml
+PYCHARM=../playbooks/pycharm_setup.yml
 
 ansible-playbook -K --become-method=su -e user=${USER} ${INITUSER}
-ansible-playbook -e user=${USER} ${PASSWORDLESS}
-ansible-playbook -e user=${USER} ${VIRTUALENV}
-ansible-playbook -e 'user=${USER} venv=manim-priv repo=git@github.com:andysizer/manim.git' ${MANIM}
+ansible-playbook -e "user=${USER}" ${PASSWORDLESS}
+ansible-playbook -e "user=${USER}" ${VIRTUALENV}
+ansible-playbook -e "user=${USER} venv=manim-priv repo=git@github.com:andysizer/manim.git" ${MANIM}
+ansible-playbook -e "user=${USER}" ${PYCHARM}
 
